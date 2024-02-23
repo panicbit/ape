5fc2349a007858aa563e604996366ae1f43d3f24
panicbit <panicbit.dev@gmail.com>
2024-02-23 12:08:48 +0100

    mitigate distorted audio

--- src/audio.rs
+++ src/audio.rs
@@ -31,9 +31,10 @@ impl Iterator for RetroAudio {
     type Item = i16;
 
     fn next(&mut self) -> Option<Self::Item> {
-        match self.current_frame.next() {
+        let sample = match self.current_frame.next() {
             Some(sample) => Some(sample),
             None => {
+                println!("should not happen more than once");
                 self.current_frame = match self.rx.recv() {
                     Ok(current_frame) => current_frame.into_iter(),
                     Err(err) => {
@@ -41,8 +42,25 @@ impl Iterator for RetroAudio {
                         return None;
                     }
                 };
+
                 self.current_frame.next()
             }
+        };
+
+        if self.current_frame.len() == 0 {
+            self.current_frame = match self.rx.recv() {
+                Ok(current_frame) => current_frame.into_iter(),
+                Err(err) => {
+                    eprintln!("Failed to receive audio frames: {err}");
+                    return None;
+                }
+            };
         }
+
+        if sample.is_none() {
+            eprintln!("returning empty sample!");
+        }
+
+        sample
     }
 }
